3267850e1bcc8c35505b5bdd0c1620fcb75324c2
k0fola <k0fola@proton.me>
2026-08-23 09:38:36 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#define WIDTH 500
+#define HEIGHT 500
+
+int main(int argc, char *argv[]){
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_Event event;
+	SDL_Texture *texture;
+	SDL_Surface *img1;
+	char name[20];
+	if(!SDL_Init(SDL_INIT_VIDEO)){
+		printf("failed to initialise video");
+		return 3;
+	}
+	if(!SDL_CreateWindowAndRenderer("Testing",WIDTH,HEIGHT,SDL_WINDOW_RESIZABLE,&window,&renderer)){
+		printf("Failed to create window and renderer");
+		return 3;
+	}
+	printf("Insert the filename of the png: ");
+	scanf("%s",&name);
+	img1 = SDL_LoadPNG(name);
+	texture = SDL_CreateTextureFromSurface(renderer,img1);
+	while(1){
+		SDL_RenderClear(renderer);
+		SDL_RenderTexture(renderer,texture,NULL,NULL);
+		SDL_RenderPresent(renderer);
+		SDL_PollEvent(&event);
+		if(event.type == SDL_EVENT_QUIT){
+			break;
+		}
+	}
+
+	return 0;
+}
+
